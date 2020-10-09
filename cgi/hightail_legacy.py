17a6c9299368be177f040951bd74985a6288addb
J5 <john.heyer@hightail.com>
2020-10-08 20:12:46 -0700

    asd

--- cgi/hightail_legacy.py
+++ cgi/hightail_legacy.py
@@ -318,9 +318,9 @@ if __name__ == "__main__":
         print("Set-Cookie: {}".format(http_response['cookies']))
 
     if 301 <= http_response['status_code'] <= 302:
-        print("Location: {}".format(http_response['location']))
+        print("Location: {}\n".format(http_response['location']))
     else:
-        print("Content-Type: {}".format(http_response['content_type']))
+        print("Content-Type: {}\n".format(http_response['content_type']))
 
     if 'body' in http_response and http_response['body']:
         if type(http_response['body']) == str:
