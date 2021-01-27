c7af359805a53a5497f86c16e826023bdf17475a
J5 <john.heyer@hightail.com>
2021-01-26 18:32:58 -0800

    error handling

--- lib/logfile.py
+++ lib/logfile.py
@@ -1,21 +1,25 @@
 class LogFile():
 
-        def __init__(self, filename, filter):
-                self.filename = filename
-                self.ReadFile(filter)
+    def __init__(self, filename, filter = None):
 
-        # Open file read-only 
-        def ReadFile(self, filter):
-                try:
-                        fh = open(self.filename,"r")
-                except:
-                        sys.exit("ERROR: could not read log file '" + self.filename + "'")
-                # Read line by line and split by space in to array
-                self.contents = []
-                for line in fh:
-                        if filter in line:
-                                parts = line.split(" ")
-                                self.contents.append(parts)
-                fh.close()
+        self.filename = filename
+        self.ReadFile(filter)
 
+    def ReadFile(self, filter = None):
 
+        try:
+            fh = open(self.filename,"r")
+        except:
+            raise Exception("ERROR: could not read log file '" + self.filename + "'")
+
+        self.contents = []
+        for line in fh:
+            if filter:    
+                if filter in line:
+                    parts = line.split(" ")
+                    self.contents.append(parts)
+            else:
+                parts = line.split(" ")
+                self.contents.append(parts)
+
+        fh.close()
